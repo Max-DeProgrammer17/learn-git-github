d82af419c8f746081279e21d258a82a3aa9b23c0
Efoe Max <efoemaxi17@gmail.com>
2025-03-22 18:42:39 +0100

    learning

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ let btn = document.querySelector(".btn");
 btn.addEventListener("click", ()=>{
     document.querySelector("body").style.backgroundColor = "green";
 })
+
+let person = {
+    name: "efoe Max",
+    age: 17,
+    school: "university of buea"
+}
+console.log(person);
+
+person = JSON.stringify(person);
+console.log(person);
+
+person = JSON.parse(person);
+console.log(person);
