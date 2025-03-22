bb58bac49544e9662d93449adeaea98a1e73bb64
Efoe Max <efoemaxi17@gmail.com>
2025-03-22 19:41:06 +0100

    fetching text file using fetch and promises locally without api

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 "use strict";
 let btn = document.querySelector(".btn");
 btn.addEventListener("click", ()=>{
-    document.querySelector("body").style.backgroundColor = "green";
+    fetch("sample.txt")
+    .then((res) => res.text())
+    .then((data) => {
+        document.getElementById("text").innerHTML = data;
+    })
 })
 
 // let person = {
@@ -17,21 +21,22 @@ btn.addEventListener("click", ()=>{
 // person = JSON.parse(person);
 // console.log(person);
 
-let xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function(){
-let list = document.getElementById("list");
-    if(this.readyState == 4 && this.status == 200){
-       let response = JSON.parse(xhr.responseText);
-        let people = response.people;
-          console.log(response.people);
+// let xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function(){
+// let list = document.getElementById("list");
+//     if(this.readyState == 4 && this.status == 200){
+//        let response = JSON.parse(xhr.responseText);
+//         let people = response.people;
+//           console.log(response.people);
 
-       let output = " ";
-       for(let i = 0; i < people.length; i++){
-        output += "<li>" +response.people[i].name+ "</li>";
-       }
-       list.innerHTML = output;
+//        let output = " ";
+//        for(let i = 0; i < people.length; i++){
+//         output += "<li>" +response.people[i].name+ "</li>";
+//        }
+//        list.innerHTML = output;
 
-    }
-};
-xhr.open("GET", "index.json", true);
-xhr.send();
+//     }
+// };
+// xhr.open("GET", "index.json", true);
+// xhr.send();
+
