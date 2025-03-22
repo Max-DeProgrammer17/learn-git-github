f16cde94226d6eef6f51c8f360710e9b86b3d49c
Efoe Max <efoemaxi17@gmail.com>
2025-03-22 19:02:26 +0100

    to json and back

--- index.js
+++ index.js
@@ -4,15 +4,26 @@ btn.addEventListener("click", ()=>{
     document.querySelector("body").style.backgroundColor = "green";
 })
 
-let person = {
-    name: "efoe Max",
-    age: 17,
-    school: "university of buea"
-}
-console.log(person);
+// let person = {
+//     name: "efoe Max",
+//     age: 17,
+//     school: "university of buea"
+// }
+// console.log(person);
 
-person = JSON.stringify(person);
-console.log(person);
+// person = JSON.stringify(person);
+// console.log(person);
 
-person = JSON.parse(person);
-console.log(person);
+// person = JSON.parse(person);
+// console.log(person);
+
+let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function(){
+
+    if(this.readyState == 4 && this.status == 200){
+       let response = xhr.responseText
+       console.log(response.people);
+    }
+};
+xhr.open("GET", "index.json", true);
+xhr.send();
