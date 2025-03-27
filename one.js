5f06b7a5777de8fb54238e06240537b8b57afd0d
Efoe Max <efoemaxi17@gmail.com>
2025-03-27 18:13:35 +0100

    learning export and import

--- one.js
+++ one.js
@@ -1,1 +1,10 @@
 "use strict";
+const mul = (a,b) => {
+    return a*b;
+}
+
+const div = (a,b) => {
+    return a/b;
+}
+
+export{mul, div}
--- two.js
+++ two.js
@@ -1,1 +1,5 @@
 "use strict";
+import{mul, div} from "./one.js";
+
+console.log(mul(2,3));
+console.log(div(10,5));
